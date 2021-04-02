bab22c53e0ed7e0806e8068c758377a8ee225c37
ClancyGilroy <78936857+TinyVail@users.noreply.github.com>
2021-04-02 18:07:15 -0500

    commit before going a diofferent route

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+// declare the variables 
+let LockButton = $(".LockButton");
+let currentHour = 
+//
+// row based data attributes
+$("#8Row").attr("data-time", moment("8:00 am", "h:mm a").format("HH"));
+$("#9Row").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+$("#10Row").attr("data-time", moment("10:00 am", "hh:mm a").format("HH"));
+$("#11Row").attr("data-time", moment("11:00 am", "hh:mm a").format("HH"));
+$("#12Row").attr("data-time", moment("12:00 pm", "hh:mm a").format("HH"));
+$("#1Row").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+$("#2Row").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+$("#3Row").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+$("#4Row").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+$("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
