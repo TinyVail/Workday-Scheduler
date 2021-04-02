c03bb508ccfeb1687b16115ad3d57bb09be24383
ClancyGilroy <78936857+TinyVail@users.noreply.github.com>
2021-04-02 18:31:50 -0500

    commiting after for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,23 @@
-// declare the variables 
+
+
+let timeContainer = $("#timeContainer");
+let timeContainerHTML = "";
+
+for (let i = 8; i <= 12; i++) {
+    timeContainerHTML = timeContainerHTML + `
+    <div class="row">
+		<div class="col-md-1 hour pt-4">${i}AM</div>
+			<input id="8Row" class="user-event-input col-md-10 event-input-block">
+		<div class="col-md-1 saveBtn" data-hour=${i}><i class="fas fa-lock" aria-hidden="true"></i></div>
+  	</div>	
+	`;
+}
+timeContainer.html("<p>Hi</p>");
+
+/**
+// declare the variables
 let LockButton = $(".LockButton");
-let currentHour = 
+let currentHour =
 //
 // row based data attributes
 $("#8Row").attr("data-time", moment("8:00 am", "h:mm a").format("HH"));
@@ -13,3 +30,5 @@ $("#2Row").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
 $("#3Row").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
 $("#4Row").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
 $("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+
+*/
